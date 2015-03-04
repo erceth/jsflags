6fff13bf8a52942a10fd9de01672e8c242717982
erceth <erceth@gmail.com>
2015-03-04 13:17:34 -0700

    proof of concept

---
+++ index.js
@@ -0,0 +1,43 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+app.get('/main.js', function(req, res) {
+    res.sendFile(__dirname + "/public/main.js");
+});
+
+
+http.listen(8001, function() {
+    console.log('listening on *:8001');
+});
+
+var gameState = [{
+    name: "block",
+    position: {
+        x: 30,
+        y: 30
+    },
+    size: {
+        height: 40,
+        width: 40
+    }
+}];
+
+setInterval(update, 1000 / 60);
+
+function update() {
+    io.emit("refresh", gameState);
+}
+
+io.on("connection", function(socket) {
+    console.log("a user connected");
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+    socket.on("keyboard", function(position) {
+        gameState[0].position.x = gameState[0].position.x + position;
+    });
+});
