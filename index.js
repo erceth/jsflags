95c23e4268ac29cb99667c84e068f694632c80d1
erceth <erceth@gmail.com>
2015-03-05 13:48:44 -0700

    resolving movement

--- index.js
+++ index.js
@@ -15,21 +15,64 @@ http.listen(8001, function() {
     console.log('listening on *:8001');
 });
 
-var gameState = [{
-    name: "block",
-    position: {
-        x: 30,
-        y: 30
-    },
-    size: {
-        height: 40,
-        width: 40
-    }
-}];
+var map = {
+	name: "squares",
+	dimensions: {width: 900, height: 900}
+};
+
+var options = {
+	numOfTanks: 1,
+};
+
+var gameState = {
+	map: map,
+	tanks: []
+}
+
+var Tank = function() {
+	this.size = {height: 15, width: 15};
+	this.position = {x: 200, y: 200};
+	this.positionStep = {x: 0, y: 0};
+	this.angle = 90; //0 to 359
+	this.speed = .1; //0 to 1
+	this.angelVel = 1; //-1 to 1
+};
+
+Tank.prototype = {
+	update: function() {
+		this.angle += this.angelVel;
+		//radians = degrees * (pi/180)
+		var radians = this.angle * (Math.PI/180);
+		this.positionStep.x += Math.cos(radians);
+		this.positionStep.y += Math.sin(radians);
+
+		console.log(this.positionStep.x, this.positionStep.y);
+		if (this.positionStep.x > 0 && this.positionStep.x < 900) {
+			this.position.x = this.positionStep.x;
+		}
+		if (this.positionStep.y > 0 && this.positionStep.y < 900) {
+			this.position.y = this.positionStep.y;
+		}
+		
+	}
+};
+
+//create tanks
+for (var i = 0; i < options.numOfTanks; i++) {
+	gameState.tanks.push(new Tank);
+}
+
+function updateTanks() {
+	for(var i = 0; i < gameState.tanks.length; i++) {
+		gameState.tanks[i].update();
+	}
+}
+
 
 setInterval(update, 1000 / 60);
 
 function update() {
+	updateTanks();
     io.emit("refresh", gameState);
 }
 
