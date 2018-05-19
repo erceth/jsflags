723d783dfa5d8622583a344f5d1780adf9f1a13a
Eric <erceth@gmail.com>
2018-05-19 14:38:32 -0500

    removed wait function

--- public/main.js
+++ public/main.js
@@ -292,23 +292,6 @@ window.onload = function () {
   var gameScreen = new GameScreen(manualControls)
 }
 
-// handy waiting function
-function wait (timesToCheck, timeToWait, isItTimeYetFn, scope, callback) {
-  var self = this
-  timesToCheck -= 1
-  if (timesToCheck <= 0) {
-    console.log('time expired')
-    return
-  }
-  setTimeout(function () {
-    if (isItTimeYetFn.call(scope)) {
-      callback()
-    } else {
-      self.wait(timesToCheck, timeToWait, isItTimeYetFn, scope, callback)
-    }
-  }, timeToWait)
-}
-
 function round (value, decimals) {
   return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals)
 }
